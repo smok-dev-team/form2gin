f381050ac8c62828bc4f704a34cb3ae9b0870d85
SmartWalle <smartwalle@gmail.com>
2017-02-28 08:47:56 +0800

    update

--- form.go
+++ form.go
@@ -3,27 +3,23 @@ package form2gin
 import (
 	"github.com/gin-gonic/gin"
 	f "github.com/smartwalle/form"
-	v "github.com/smartwalle/validator"
 	"reflect"
 	"sync"
 )
 
 const k_FORM_ERROR = "_f2g_bind_form_error"
-const k_VALIDATE_ERROR = "_f2g_validate_form_error"
 const k_BIND_FORM = "_f2g_form"
-const k_DEFAULT_KEY = "Default"
+const k_DEFAULT_KEY = "_f2g_default"
 
 type Handler interface{}
 type ErrorHandler func(c *gin.Context, err error)
 
 var bindErrorHandlers map[string]ErrorHandler
-var validateErrorHandlers map[string]ErrorHandler
 var once sync.Once
 
 func init() {
 	once.Do(func() {
 		bindErrorHandlers = make(map[string]ErrorHandler)
-		validateErrorHandlers = make(map[string]ErrorHandler)
 	})
 }
 
@@ -33,14 +29,6 @@ func RegisterBindErrorHandler(handler ErrorHandler) {
 
 func RegisterBindErrorHandlerWithKey(key string, handler ErrorHandler) {
 	bindErrorHandlers[key] = handler
-}
-
-func RegisterValidateErrorHandler(handler ErrorHandler) {
-	RegisterValidateErrorHandlerWithKey(k_DEFAULT_KEY, handler)
-}
-
-func RegisterValidateErrorHandlerWithKey(key string, handler ErrorHandler) {
-	validateErrorHandlers[key] = handler
 }
 
 // ================================================================================
@@ -64,40 +52,7 @@ func MidBindFormWithKey(key string, form interface{}) gin.HandlerFunc {
 				bindErrorHandler(c, err)
 			}
 			c.Set(k_FORM_ERROR, err)
-		}
-		c.Set(k_BIND_FORM, newForm.Interface())
-		c.Next()
-	}
-}
-
-func MidBindAndValidateForm(form interface{}) gin.HandlerFunc {
-	return MidBindAndValidateFormWithKey(k_DEFAULT_KEY, form)
-}
-
-func MidBindAndValidateFormWithKey(key string, form interface{}) gin.HandlerFunc {
-	var formType = reflect.TypeOf(form)
-	if formType.Kind() == reflect.Ptr {
-		formType = formType.Elem()
-	}
-
-	var bindErrorHandler = bindErrorHandlers[key]
-	var validateErrorHandler = validateErrorHandlers[key]
-
-	return func(c *gin.Context) {
-		var newForm = reflect.New(formType)
-		var err = f.BindWithRequest(c.Request, newForm.Interface())
-		if err != nil {
-			if bindErrorHandler != nil {
-				bindErrorHandler(c, err)
-			}
-			c.Set(k_FORM_ERROR, err)
-		}
-		var val = v.LazyValidate(newForm.Interface())
-		if val.OK() == false {
-			if validateErrorHandler != nil {
-				validateErrorHandler(c, val.Error())
-			}
-			c.Set(k_VALIDATE_ERROR, val.Error())
+			return
 		}
 		c.Set(k_BIND_FORM, newForm.Interface())
 		c.Next()
@@ -127,25 +82,16 @@ func HandlerWrapper(h Handler) gin.HandlerFunc {
 
 // ================================================================================
 
-func BindAndValidateForm(c *gin.Context, form interface{}) bool {
-	return BindAndValidateFormWithKey(k_DEFAULT_KEY, c, form)
+func BindForm(c *gin.Context, form interface{}) bool {
+	return BindFormWithKey(k_DEFAULT_KEY, c, form)
 }
 
-func BindAndValidateFormWithKey(key string, c *gin.Context, form interface{}) bool {
+func BindFormWithKey(key string, c *gin.Context, form interface{}) bool {
 	var err = f.BindWithRequest(c.Request, form)
 	if err != nil {
 		var bindErrorHandler = bindErrorHandlers[key]
 		if bindErrorHandler != nil {
 			bindErrorHandler(c, err)
-		}
-		return false
-	}
-
-	var val = v.LazyValidate(form)
-	if val.OK() == false {
-		var validateErrorHandler = validateErrorHandlers[key]
-		if validateErrorHandler != nil {
-			validateErrorHandler(c, val.Error())
 		}
 		return false
 	}
